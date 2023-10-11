9f7b3f381c67576cb3992e7118a4b02effb95ce5
khalid khan <khalidkhanali550@gmail.com>
2023-10-11 15:56:32 +0500

    first commit

---
+++ lib/client/Home/Reservation.dart
@@ -0,0 +1,248 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:maps_toolkit/maps_toolkit.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class RestaurantHomePage extends StatefulWidget {
+  @override
+  _RestaurantHomePageState createState() => _RestaurantHomePageState();
+}
+
+class _RestaurantHomePageState extends State<RestaurantHomePage> {
+  bool isDataLoaded = false;
+  var parsedData;
+  TextEditingController searchController = TextEditingController();
+  List<Map<String, dynamic>> data = [];
+  List<Map<String, dynamic>> originalData = [];
+  List<Map<String, dynamic>> restaurantsWithin10Km = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'HOME',
+          style: TextStyle(fontFamily: 'Pacifico'),
+        ),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              calculateAndFilterRestaurantsWithin10Km();
+            },
+            child: Text("Calculate Distance"),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                hintText: 'Search for food...',
+                prefixIcon: Icon(Icons.search),
+              ),
+              onChanged: filterData,
+            ),
+          ),
+          Expanded(
+            child: isDataLoaded
+                ? buildDataList(restaurantsWithin10Km.isNotEmpty
+                ? restaurantsWithin10Km
+                : data)
+                : Center(child: CircularProgressIndicator()),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> calculateAndFilterRestaurantsWithin10Km() async {
+    try {
+      // Get the customer's current location
+      final Position customerLocation = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best,
+      );
+
+      // Clear the previous list of restaurants within 10 km
+      restaurantsWithin10Km.clear();
+
+      for (var restaurant in data) {
+        // Use geocoding to convert the restaurant's address into coordinates
+        List<Location> locations =
+        await locationFromAddress(restaurant['address']);
+        if (locations.isNotEmpty) {
+          double restaurantLatitude = locations[0].latitude;
+          double restaurantLongitude = locations[0].longitude;
+
+          // Calculate the distance between the customer's location and the restaurant
+          double distance = Geolocator.distanceBetween(
+            customerLocation.latitude,
+            customerLocation.longitude,
+            restaurantLatitude,
+            restaurantLongitude,
+          );
+
+          // Print the distance (in meters)
+          double distanceIntoKiloMeter = distance / 1000;
+
+          if (distanceIntoKiloMeter <= 10) {
+            // Add the restaurant to the list of restaurants within 10 km
+            restaurantsWithin10Km.add(restaurant);
+          }
+
+          print('Distance to restaurant: $distanceIntoKiloMeter kilo meters');
+        } else {
+          print('Unable to determine restaurant coordinates from address.');
+        }
+      }
+
+      // Update the UI with the filtered data
+      setState(() {});
+    } catch (e) {
+      print('Error during geocoding or distance calculation: $e');
+    }
+  }
+
+  Future<void> fetchData() async {
+    try {
+      final response = await http.get(
+          Uri.parse('http://192.168.0.102:8080/customer/signin/home'));
+
+      if (response.statusCode == 200) {
+        parsedData = parseJsonResponse(response.body);
+        setState(() {
+          data = parsedData;
+          originalData = parsedData;
+          isDataLoaded = true;
+        });
+      } else {
+        print('Failed to fetch data: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error while fetching data: $e');
+    }
+  }
+
+  void filterData(String query) {
+    if (query.isEmpty) {
+      setState(() {
+        data = originalData;
+      });
+    } else {
+      final filteredData = originalData.where((item) {
+        final name = item['name'].toString().toLowerCase();
+        final address = item['address'].toString().toLowerCase();
+        return name.contains(query.toLowerCase()) ||
+            address.contains(query.toLowerCase());
+      }).toList();
+      setState(() {
+        data = filteredData;
+      });
+    }
+  }
+
+  List<Map<String, dynamic>> parseJsonResponse(String responseBody) {
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed;
+  }
+
+  Widget buildDataList(List<Map<String, dynamic>> data) {
+    return ListView.builder(
+      itemCount: data.length,
+      itemBuilder: (context, index) {
+        final item = data[index];
+        return Card(
+          color: Colors.teal,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(25),
+            ),
+          ),
+          elevation: 4.0,
+          child: Container(
+            width: 120,
+            height: 120,
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            CircleAvatar(
+                              radius: 45,
+                              backgroundColor: Colors.white,
+                              backgroundImage:
+                              AssetImage("Assets/images/13.png"),
+                              child: Text(
+                                item['name'][0],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 36.0,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 7,
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "Name: ${item['name']}",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14.0,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Address: ${item['address']}",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import './client/./client_signin/loginpg.dart';
+import './restaurant/./profile/profile.dart';
+import './restaurant/./signin/./loginpg.dart';
+
+void main() {
+  runApp(const MyHomePage());
+  // runApp(const MyApp());
+}
+String? token;
+String? name;
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       // Use FutureBuilder to check if the user is authenticated or not
+//       home: FutureBuilder<bool>(
+//         future: checkAuthStatus(),
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             // Waiting for Future<bool> to complete
+//             return const CircularProgressIndicator();
+//           } else if (snapshot.hasError || snapshot.data == false) {
+//             // Error or user not authenticated
+//             return const MyHomePage(); // Redirect to Login
+//           } else {
+//             // User is authenticated
+//           return Profile(name: name!, token: token!);
+//           }
+//         },
+//       ),
+//     );
+//   }
+//
+// // Function to check if the user is authenticated
+//   Future<bool> checkAuthStatus() async {
+//     final prefs = await SharedPreferences.getInstance();
+//     token = prefs.getString('auth_token');
+//     print("token is:::${token}");
+//     // Check if the token exists and is valid (you need to implement this logic)
+//     return token != null;
+//   }
+// }
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xFF020403),
+
+
+            bottom: const TabBar(tabs: [
+              Tab(text: "Restaurant"),
+              Tab(text: "Customer"),
+            ]),
+            title: Text("Dine Easy"),
+          ),
+          body: const TabBarView(children: [
+            Home_2(),
+            Home_1(),
+          ]),
+        ),
+      ),
+    );
+  }
+}
